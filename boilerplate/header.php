ca5e35b6f08a4657606f3ec6f0ab88d62c24498b
andrew-macpherson <andrewmac17@gmail.com>
2014-03-29 08:29:18 -0700

    Starting point

---
+++ boilerplate/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php wp_title(); ?></title>
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>"
+
+  <?php wp_head(); ?>
+</head>
+
+<body>
