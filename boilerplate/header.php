7121cadcfb0df04e825cd0fc6fd716b5bf59e38e
Andrew MacPherson <andrewmac17@gmail.com>
2014-03-29 08:29:59 -0700

    Update header.php

--- boilerplate/header.php
+++ boilerplate/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title><?php wp_title(); ?></title>
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>"
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 
   <?php wp_head(); ?>
 </head>
